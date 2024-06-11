f72cfb5163d49ec69a0eee6e0957808407e0ce18
Luffy20241 <168181882+Luffy20241@users.noreply.github.com>
2024-06-11 13:53:20 +0600

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -341,7 +341,7 @@ class MirrorLeechListener:
         user_id = self.message.from_user.id
         name, _ = await process_file(name, user_id, isMirror=not self.isLeech)
         user_dict = user_data.get(user_id, {})
-        msg = f'<blockquote>🅓🅞🅝🅔</blockquote>\n\n'
+        msg = f'🅓🅞🅝🅔\n\n'
         msg += f'<blockquote><b>• Size: </b>{get_readable_file_size(size)}\n'
         msg += f'<b>• Elapsed: </b>{get_readable_time(time() - self.message.date.timestamp())}\n'
         LOGGER.info(f'Task Done: {name}')
