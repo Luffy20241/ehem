8881f2e8785e56e8e386b727aaf13b65cedf9b81
Luffy20241 <168181882+Luffy20241@users.noreply.github.com>
2024-06-17 03:15:14 +0600

    Update nsfw_check.py

---
+++ bot/helper/aeon_utils/nsfw_check.py
@@ -0,0 +1,74 @@
+from bot import LOGGER
+from re import IGNORECASE, search, escape
+
+
+nsfw_keywords = [
+    "poooooooorn",
+    "onnnnnnnlyfans",
+    "nsffffffw",
+    "Brvvhjjjgazzers",
+    "aduuuuuuuult",
+    "xnnnnnnnnnnnxx",
+    "xvbhjjjhhvvcideos",
+    "nsfwchghuudyuferry",
+    "hardcgjjjuhffdore",
+    "Pornvuyffyhvchub",
+    "xvighuiiigfdeos2",
+    "youpvvhugcggorn",
+    "pornrvhjjcxxxip",
+    "plagghiigvcyboy",
+    "henvvhihccvtai",
+    "erotvvbjkvccica",
+    "blowvvhjjgccjob",
+    "redtvbjjbvccube",
+    "stripvhijfchbchat",
+    "camgggjhcfhjirl",
+    "nugiifgjde",
+    "fetghkjvcccish",
+    "cucggjkkvckold",
+    "orvhhkkkbgy",
+    "horgyijhhcccny",
+    "swinfghkifdsdfggers",
+]
+
+
+def isNSFW(text):
+    pattern = r'(?:^|\W|_)(?:' + '|'.join(escape(keyword) for keyword in nsfw_keywords) + r')(?:$|\W|_)'
+    return bool(search(pattern, text, flags=IGNORECASE))
+
+
+def isNSFWdata(data):
+    if isinstance(data, list):
+        for item in data:
+            if isinstance(item, dict):
+                if any(isinstance(value, str) and isNSFW(value) for value in item.values()):
+                    return True
+            elif 'name' in item and isinstance(item['name'], str) and isNSFW(item['name']):
+                return True
+    elif isinstance(data, dict) and 'contents' in data:
+        for item in data['contents']:
+            if 'filename' in item and isNSFW(item['filename']):
+                return True
+    return False
+
+
+async def nsfw_precheck(message):
+    if isNSFW(message.text):
+        return True
+    
+    reply_to = message.reply_to_message
+    if not reply_to:
+        return False
+
+    if any(
+        isNSFW(getattr(reply_to, attr).file_name)
+        for attr in ['document', 'video']
+        if hasattr(reply_to, attr) and getattr(reply_to, attr)
+    ):
+        return True
+
+    return any(
+        isNSFW(getattr(reply_to, attr))
+        for attr in ['caption', 'text']
+        if hasattr(reply_to, attr) and getattr(reply_to, attr)
+    )
